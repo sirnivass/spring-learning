9b7845fb19a80ce7f761387ee7ebb7b9df70d838
sirnivass <sirnivass@gmail.com>
2025-02-17 09:25:52 -0300

    Feat: implementa um endpoint Category e implementa contrutores em Category

--- src/main/java/com/devsuperior/models/Category.java
+++ src/main/java/com/devsuperior/models/Category.java
@@ -17,7 +17,16 @@ public class Category{
         return id;
     }
     
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     // equals e hashCode são métodos de comparação entre as categorias.
+    //Método rápido, que consegue varrer uma coleção enorme!
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -25,7 +34,7 @@ public class Category{
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
-
+    // Método lento mas que compara "100%"
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -42,6 +51,4 @@ public class Category{
             return false;
         return true;
     }
-    
-    
 }
