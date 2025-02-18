027255f63ed63d71ec0c1e50758928cbeb272e4b
sirnivass <sirnivass@gmail.com>
2025-02-17 22:05:28 -0300

    feat: cria interface repository e adiciona anotações na entidade Category

--- src/main/java/com/devsuperior/models/Category.java
+++ src/main/java/com/devsuperior/models/Category.java
@@ -1,17 +1,21 @@
 package com.devsuperior.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "category")
 public class Category{
 
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
     public Long getId() {
         return id;
---
+++ src/main/java/com/devsuperior/repositories/CategoryRepositories.java
@@ -0,0 +1,10 @@
+package com.devsuperior.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.models.Category;
+
+public interface CategoryRepositories extends JpaRepository<Category, Long>{
+
+
+}
