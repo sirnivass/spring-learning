a9e2322b46721b47da73372425d868e584a68b9c
sirnivass <sirnivass@gmail.com>
2025-02-17 23:08:42 -0300

    Remove a lista mokada e Instancia service

--- src/main/java/com/devsuperior/controllers/CategoryController.java
+++ src/main/java/com/devsuperior/controllers/CategoryController.java
@@ -1,6 +1,5 @@
 package com.devsuperior.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -9,17 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.models.Category;
+import com.devsuperior.services.CategoryService;
 
 @RestController
 @RequestMapping("/api")
 public class CategoryController {
 
+    //Assim como detalhei em CategoryService meu controller "chama" uma instância de service que trás consigo os métodos que lá constam.
+    private final CategoryService service;
+    
+    public CategoryController(CategoryService service){
+        this.service = service;
+    }
+    
     @GetMapping
     public ResponseEntity<List<Category>> findAll(){
-        List<Category> list = new ArrayList<>();
-        list.add(new Category(1L, "Books"));
-        list.add(new Category(2L, "Eletronics"));
-
+        List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
