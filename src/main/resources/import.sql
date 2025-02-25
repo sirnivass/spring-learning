6e9bf217e025a3a029278e4d9a14c8bbb415000d
sirnivass <sirnivass@gmail.com>
2025-02-25 19:59:38 -0300

    Cria um import para popular o banco automaticamente.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+/*Por algum motivo ele não aceitou eu digitar assim:
+INSERT INTO category(name) 
+VALUES 
+('Books'), 
+('Computers'), 
+('Electronics');
+*/
+
+INSERT INTO category(name) VALUES ('Books'), ('Computers'), ('Electronics');
