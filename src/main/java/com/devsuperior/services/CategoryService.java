6958e7a5423f6c1e15903a83611e073a771a706a
sirnivass <sirnivass@gmail.com>
2025-02-26 09:34:33 -0300

    Atualiza o Service para trabalhar com DTO

--- src/main/java/com/devsuperior/services/CategoryService.java
+++ src/main/java/com/devsuperior/services/CategoryService.java
@@ -2,31 +2,61 @@ package com.devsuperior.services;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dtos.CategoryDTO;
 import com.devsuperior.models.Category;
 import com.devsuperior.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
 
-    private final CategoryRepository repository; //Preciso de uma instancia de repository aqui em services para que, o service tenha acesso aos métodos
-    // o atributo repository não pode ser alterado após ser atribuído. 
-    //Isso significa que, uma vez que o objeto CategoryRepositories seja injetado no construtor, ele não poderá ser substituído por outro objeto
+
+    private final CategoryRepository repository; 
+
     public CategoryService(CategoryRepository repository) {
         this.repository = repository;
-        //Atribui o valor do parâmetro (CategoryRepository) ao atributo (repository).
     }
-    //A partir desse momento repository possui
-    //findAll(), findById(Long id), save(Category entity), deleteById(Long id)...
 
-    /* 
-    * Como faço para que esse método consiga acessar o Repository e chamar lá no BD as categorias?
-    * Para tanto, preciso injetar uma dependencia aqui no service do Repository
-    */
-    public List<Category> findAll(){ 
-        return repository.findAll(); //Aqui consigo ver os métodos JPA que repository herdou
-        
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll(){ 
+        List<Category> list = repository.findAll();
+        return list.stream()
+            .map(CategoryDTO::new)
+            .collect(Collectors.toList());
     }
+    /*  
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll(){ 
+    //Essa fincao espera um reotorno do tipo lista
+    //O tipo da Lista é um DTO
+    //Mas, o Service só trabalha com model
+    //Então declaramos uma lista de Category:
+        List<Category> list = repository.findAll();
+    //Após declarado, vamos usar stream map para converter em DTO
+        List<CategoryDTO> listDto = list.stream()//stream permite trabalhar com funcoes de alta ordem [Lambda, nesse caso vamos trabalhar com map]
+        .map(x - > new CategoryDTO(x)) //stream aplica uma funcao a cada elemento da nossa lista[nesse caso específico]
+        .collect(Collectors.toList()); //após transformar lista num map, temos que voltar para lista, usando collect
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Resumindo:
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll(){ 
+        List<Category> list = repository.findAll();
+        return list.stream().map(x - > new CategoryDTO(x)).collect(Collectors.toList());
+        //Antes, eu declarava a lista, fazia a conversao e depois reotrnava a lista, agora já retorno direto.
+
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll(){ 
+        List<Category> list = repository.findAll();
+        
+        List<CategoryDTO> listDto = new ArrayList<>();
+        for (Category cat : list){
+        listDto.add(new CategoryDTO(cat));
+
+        return listDto; 
+        }
+    */
 }
